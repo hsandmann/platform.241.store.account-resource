d9bca5cfbcecbf5790ed6e04133706b55463f505
Humberto Sandmann <hsandmann@ieee.org>
2024-02-21 11:05:47 -0300

    new version

--- src/main/java/insper/store/account/AccountResource.java
+++ src/main/java/insper/store/account/AccountResource.java
@@ -1,5 +1,8 @@
 package insper.store.account;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountResource implements AccountController {
 
     @GetMapping("/accounts/info")
-    public String info() {
-        return "Account service";
+    public ResponseEntity<Map<String, String>> info() {
+        return new ResponseEntity<Map<String, String>>(
+            Map.ofEntries(
+                Map.entry("microservice.name", AccountApplication.class.getSimpleName()),
+                Map.entry("os.arch", System.getProperty("os.arch")),
+                Map.entry("os.name", System.getProperty("os.name")),
+                Map.entry("os.version", System.getProperty("os.version")),
+                Map.entry("file.separator", System.getProperty("file.separator")),
+                Map.entry("java.class.path", System.getProperty("java.class.path")),
+                Map.entry("java.home", System.getProperty("java.home")),
+                Map.entry("java.vendor", System.getProperty("java.vendor")),
+                Map.entry("java.vendor.url", System.getProperty("java.vendor.url")),
+                Map.entry("java.version", System.getProperty("java.version")),
+                Map.entry("line.separator", System.getProperty("line.separator")),
+                Map.entry("path.separator", System.getProperty("path.separator")),
+                Map.entry("user.dir", System.getProperty("user.dir")),
+                Map.entry("user.home", System.getProperty("user.home")),
+                Map.entry("user.name", System.getProperty("user.name")),
+                Map.entry("jar", new java.io.File(
+                    AccountApplication.class.getProtectionDomain()
+                        .getCodeSource()
+                        .getLocation()
+                        .getPath()
+                    ).toString())
+            ), HttpStatus.OK
+        );
     }
 
     @Override
